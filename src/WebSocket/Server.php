d4a5fc618e55295b3c4b0dd989c9a91eb6cad5e7
simon <crcms@crcms.cn>
2018-12-21 15:23:32 +0800

    Add http support

--- src/WebSocket/Server.php
+++ src/WebSocket/Server.php
@@ -7,6 +7,7 @@ use CrCms\Server\WebSocket\Events\CloseEvent;
 use CrCms\Server\WebSocket\Events\MessageEvent;
 use CrCms\Server\WebSocket\Events\OpenEvent;
 use Swoole\WebSocket\Server as WebSocketServer;
+use CrCms\Server\Http\Events\RequestEvent;
 
 /**
  * Class Server
@@ -18,6 +19,7 @@ class Server extends HttpServer
      * @var array
      */
     protected $events = [
+        'request' => RequestEvent::class,
         'open' => OpenEvent::class,
         'message' => MessageEvent::class,
         'close' => CloseEvent::class,
