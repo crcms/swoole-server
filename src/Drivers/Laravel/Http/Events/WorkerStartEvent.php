00c3561fa73aa5ffacc65dd7c951b05e637923d0
crcms <crcms@crcms.cn>
2019-02-21 19:18:17 +0800

    chore: clear

--- src/Drivers/Laravel/Http/Events/WorkerStartEvent.php
+++ src/Drivers/Laravel/Http/Events/WorkerStartEvent.php
@@ -5,8 +5,6 @@ namespace CrCms\Server\Drivers\Laravel\Http\Events;
 use function CrCms\Server\clear_opcache;
 use CrCms\Server\Drivers\Laravel\Http\Server;
 use CrCms\Server\Server\Events\WorkerStartEvent as BaseWorkerStartEvent;
-use Illuminate\Contracts\Container\Container;
-use Illuminate\Contracts\Http\Kernel;
 
 class WorkerStartEvent extends BaseWorkerStartEvent
 {
@@ -26,24 +24,6 @@ class WorkerStartEvent extends BaseWorkerStartEvent
 
         clear_opcache();
 
-        $app = $this->server->getApplication();
-//
-//        $this->bootstrap($app);
-//
-//        //preload sharing instance
-//        $this->server->getLaravel()->preload();
-
         $this->server->getContainer()->make('events')->dispatch('worker_start', [$this->server, $app]);
-    }
-
-    /**
-     * bootstrap
-     *
-     * @param Container $app
-     * @return void
-     */
-    protected function bootstrap(Container $app): void
-    {
-        $app->make(Kernel::class)->bootstrap();
     }
 }
