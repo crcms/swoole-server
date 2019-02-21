15b4fd39c9873c855fcb49225cf76f11b707bedf
crcms <crcms@crcms.cn>
2019-02-21 19:01:19 +0800

    test: require full laravel-framework

--- tests/Laravel/bootstrap.php
+++ tests/Laravel/bootstrap.php
@@ -1,19 +1,32 @@
 <?php
 
+//
 $app = new \Illuminate\Container\Container();
 \Illuminate\Container\Container::setInstance($app);
 \Illuminate\Support\Facades\Facade::setFacadeApplication($app);
-
+//
 $app->alias('config', \Illuminate\Contracts\Config\Repository::class);
 $app->alias('config', \Illuminate\Config\Repository::class);
 $app->alias('events', \Illuminate\Contracts\Events\Dispatcher::class);
 $app->alias('events', \Illuminate\Events\Dispatcher::class);
 
 //config
-$swooleConfig = __DIR__.'/../../config/config.php';
+$swooleConfig = require __DIR__.'/../../config/config.php';
+
+
+//$application = new \CrCms\Server\Tests\Laravel\Application($swooleConfig);
+//
+//$app = $application->app();
+
+
 $app->singleton('config', function () use ($swooleConfig) {
+    $swooleConfig['laravel']['app'] = \CrCms\Server\Tests\Laravel\Application::class;
     return new \Illuminate\Config\Repository(['swoole' => $swooleConfig]);
 });
+
+$app->instance('path.config',__DIR__.'/../../config');
+
+
 //function config_path($path = null)
 //{
 //    return is_null($path) ? __DIR__ : __DIR__.'/'.$path;
@@ -46,7 +59,7 @@ $providers = [
     \Illuminate\Events\EventServiceProvider::class,
     \CrCms\Server\Drivers\Laravel\SwooleServiceProvider::class,
 ];
-
+//
 $providers = array_map(function ($provider) use ($app) {
     return new $provider($app);
 }, $providers);
