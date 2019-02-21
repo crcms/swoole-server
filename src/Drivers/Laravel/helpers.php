d46879004c775edeee7bf41bc877e9da0a3af9f7
crcms <crcms@crcms.cn>
2019-02-21 11:22:36 +0800

    feat: add get laravel framework type and version

---
+++ src/Drivers/Laravel/helpers.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace CrCms\Server\Drivers\Laravel;
+
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Foundation\Application;
+use Laravel\Lumen\Application as LumenApplication;
+
+/**
+ * get_framework_type
+ *
+ * @param Container $app
+ * @return string
+ */
+function get_framework_type(Container $app): string
+{
+    if ($app instanceof Application) {
+        return 'Laravel';
+    } elseif ($app instanceof LumenApplication) {
+        return 'Lumen';
+    } else {
+        return 'Unknown';
+    }
+}
+
+/**
+ * get_framework_version
+ *
+ * @param Container $app
+ * @return string
+ */
+function get_framework_version(Container $app): string
+{
+    $frameworkType = get_framework_type($app);
+    if (in_array($frameworkType, ['Laravel', 'Lumen'], true)) {
+        return strval($app->version());
+    } else {
+        return 'Unknown';
+    }
+}
